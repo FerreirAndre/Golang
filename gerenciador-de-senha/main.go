cf0ae9fb1309acfae27b54a1ecb53d17fdd80d5c
FerreirAndre <adrecruz15@gmail.com>
2021-06-21 16:12:46 -0300

    atualizado

--- gerenciador-de-senha/main.go
+++ gerenciador-de-senha/main.go
@@ -12,9 +12,11 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/service/inserir", servidor.Inserir).Methods(http.MethodPost)
-	router.HandleFunc("/service/buscar",servidor.BuscarUsuarios).Methods(http.MethodGet)
-	router.HandleFunc("/service/buscar/{id}",servidor.BuscarUsuario).Methods(http.MethodGet)
+	router.HandleFunc("/service", servidor.Inserir).Methods(http.MethodPost)
+	router.HandleFunc("/service",servidor.BuscarUsuarios).Methods(http.MethodGet)
+	router.HandleFunc("/service/{id}",servidor.BuscarUsuario).Methods(http.MethodGet)
+	router.HandleFunc("/service/{id}",servidor.Editar).Methods(http.MethodPut)
+	router.HandleFunc("/service/{id}",servidor.Deletar).Methods(http.MethodDelete)
 	fmt.Println("Escutando a porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
--- gerenciador-de-senha/servidor/servidor.go
+++ gerenciador-de-senha/servidor/servidor.go
@@ -10,8 +10,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type service struct {
-	Servico  string `json:"servico"`
+type servico struct {
+	Service  string `json:"service"`
 	User     string `json:"user"`
 	Password string `json:"password"`
 }
@@ -24,9 +24,9 @@ func Inserir(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var service service
+	var service_ servico
 
-	if erro = json.Unmarshal(corpoReq, &service); erro != nil {
+	if erro = json.Unmarshal(corpoReq, &service_); erro != nil {
 		w.Write([]byte("Erro ao converte usuario para struct"))
 		return
 	}
@@ -45,16 +45,16 @@ func Inserir(w http.ResponseWriter, r *http.Request) {
 	}
 	defer statement.Close()
 
-	insercao, erro := statement.Exec(service.Servico, service.User, service.Password)
+	insercao, erro := statement.Exec(service_.Service, service_.User, service_.Password)
 	_ = insercao
 	if erro != nil {
 		w.Write([]byte("Erro ao executar o statement"))
 		return
 	}
 
-	fmt.Println("Usuario inserido.", service.Servico)
+	fmt.Println("Usuario inserido.", service_.Service)
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(fmt.Sprintf("Usuario e senha guardados com sucesso, servico: %s", service.Servico)))
+	w.Write([]byte(fmt.Sprintf("Usuario e senha guardados com sucesso, servico: %s", service_.Service)))
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
@@ -72,11 +72,11 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	}
 	defer linhas.Close()
 
-	var services []service
+	var services []servico
 	for linhas.Next() {
-		var servico service
+		var servico servico
 
-		if erro := linhas.Scan(&servico.Servico); erro != nil {
+		if erro := linhas.Scan(&servico.Service); erro != nil {
 			w.Write([]byte("Erro ao scanear o serviço"))
 			return
 		}
@@ -92,7 +92,7 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 	parametros := mux.Vars(r)
 
-	servico := parametros["id"]
+	id := parametros["id"]
 
 	db, erro := banco.Conectar()
 	if erro != nil {
@@ -100,22 +100,83 @@ func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	linha, erro := db.Query("SELECT * FROM servico WHERE service = ?", servico)
+	linha, erro := db.Query("SELECT * FROM servico WHERE service = ?", id)
 	if erro != nil {
 		w.Write([]byte("Erro ao buscar servico"))
 		return
 	}
 
-	var servico_ service
-	
+	var servico_ servico
+
 	if linha.Next() {
-		if erro = linha.Scan(&servico_.Servico, &servico_.User,&servico_.Password); erro !=nil{
+		if erro = linha.Scan(&servico_.Service, &servico_.User, &servico_.Password); erro != nil {
 			w.Write([]byte("Erro ao escanear o usuario"))
 		}
 	}
-	
-	if erro = json.NewEncoder(w).Encode(servico_);erro!=nil{
+
+	if erro = json.NewEncoder(w).Encode(servico_); erro != nil {
 		w.Write([]byte("Erro ao converter o usuario para json"))
 		return
 	}
+}
+
+func Editar(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	ID := parametros["id"]
+
+	corpoReq, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Erro ao ler o corpo da requisição"))
+		return
+	}
+	var servico servico
+
+	if erro = json.Unmarshal(corpoReq, &servico); erro != nil {
+		w.Write([]byte("Erro ao converter para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("UPDATE servico SET service = ?, user = ?, password = ? WHERE service = ?")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement"))
+	}
+	defer statement.Close()
+	if _, erro := statement.Exec(servico.Service, servico.User, servico.Password, ID); erro != nil {
+		w.Write([]byte("Erro ao atualizar informações"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func Deletar(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	id := parametros["id"]
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("DELETE FROM servico WHERE service=?")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar statement"))
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(id); erro != nil {
+		w.Write([]byte("Erro ao executar os statement"))
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
